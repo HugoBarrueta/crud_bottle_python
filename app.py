b83ef40cb449078c64cb0b4638a099963369c9a4
Hugo Barrueta <hbarruetaf97@gmail.com>
2019-12-11 01:26:37 -0600

    bug delete resolved

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from sys import argv
 from bottle import route, run, template, request, static_file, error,view
 import sqlite3
 import smtplib
+import jinja2
+import pdfkit
+import bottle_utils.form as mod
 
 db_name = 'data/products.db' #acceso a la base de datos
 
@@ -21,30 +24,30 @@ def index():
         query = "SELECT codigo, name, price, stock FROM products"
         db_rows = run_query(query)
 
-        #output = template('temp/make_table', rows = db_rows)
         output = template('temp/index', rows = db_rows)
-
         return output
-
+        
 
     except IndexError as e:
         return "error", e
-        
+     
 
 @route('/add_products', method = 'GET')
 def add_products():
     try:
         if request.GET.get('add','').strip():
-
+            form = request.GET.get('addForm')
             code = request.GET.get('code','').strip()
             name = request.GET.get('name','').strip()
             price = request.GET.get('price','').strip()
             stock = request.GET.get('stock','').strip()
-
+            #if len(code) > 0:
             query = 'INSERT INTO products VALUES(NULL, ?, ?, ?, ?)'
             parameters = (code, name, price, stock)
             run_query(query, parameters)
-
+            """else:
+                return "error"
+"""
             return template('temp/success')
         else:
             return template('temp/add')
@@ -52,6 +55,7 @@ def add_products():
 
     except IndexError as e:
         return "Error", e
+
 
 @route('/product/:no', method = 'GET')
 def edit_product(no):
@@ -84,20 +88,19 @@ def edit_product(no):
             conn = sqlite3.connect(db_name)
             c = conn.cursor()
             cod = request.GET.get('guitar','').strip()
-            c.execute("SELECT name, price, stock FROM products WHERE codigo LIKE ?", (cod, ))        
-            data = c.fetchone()
-            return template('temp/delete', old=data, no=cod)
+            print(cod)
+            c.execute("DELETE FROM products WHERE codigo = ?", (cod, ))
+            conn.commit()
 
+            return template('temp/success')
     except ValueError as e:
         return "Error", e
-        
+
 
 def mail(msg):
     message = msg
     subject = "Excepcion encontrada"
-
     message = 'Subject: {}\n\n{}'.format(subject, message)
-
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server.login('hramonbf@gmail.com', 'clzqmbayjodeslci')
@@ -105,31 +108,6 @@ def mail(msg):
     server.sendmail('hramonbf@gmail.com', 'hbarruetaf97@gmail.com', message)
 
     server.quit()
-
-    
-
-@route('/delete/:no')
-def delete(no):
-    try:
-        if request.GET.get('del','').strip():
-            conn = sqlite3.connect(db_name)
-            c = conn.cursor()
-            cod = request.GET.get('guitar','').strip()
-            new_name = request.GET.get('name','').strip()
-            new_price = request.GET.get('price','').strip()
-            new_stock = request.GET.get('stock','').strip()
-            c.execute("DELETE FROM products WHERE codigo = ? AND name= ? AND price = ? AND stock =?", (cod, new_name, new_price, new_stock, ))
-            c.fetchone()
-            #query = "DELETE FROM products WHERE codigo = ? AND name= ? AND price = ? AND stock =?"
-            #parameters = (cod, new_name, new_price, new_stock)
-            #run_query(query, parameters)
-
-            return template('temp/success')
-    except ValueError as e:
-        
-        return "error", e
-
-
 
 
 @error(404)
@@ -149,3 +127,4 @@ def error503(error):
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=argv[1])
+    #run(host='localhost', port=3000, reloader="true")
