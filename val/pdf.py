5dddda3c69566e9a2bfc7697977dba6175394d26
Hugo Barrueta <hbarruetaf97@gmail.com>
2019-12-15 02:23:05 -0600

    nueva prueba de pdf

--- val/pdf.py
+++ val/pdf.py
@@ -3,7 +3,7 @@ from xhtml2pdf import pisa             # import python module
 # Define your data
 #sourceHtml = "<html><body><p>To PDF or not to PDF</p></body></html>"
 #outputFilename = "test.pdf"
-
+"""
 # Utility function
 def convertHtmlToPdf(sourceHtml, outputFilename):
     # open output file for writing (truncated binary)
@@ -24,4 +24,18 @@ def convertHtmlToPdf(sourceHtml, outputFilename):
 # Main program
 #if __name__ == "__main__":
  #   pisa.showLogging()
-    #convertHtmlToPdf(sourceHtml, outputFilename)
+    #convertHtmlToPdf(sourceHtml, outputFilename)"""
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from bottle import response
+def convertHtmlToPdf(pdf):
+    response.headers['Content-Type'] = 'application/pdf; charset=UTF-8'
+    response.headers['Content-Disposition'] = 'attachment; filename="test.pdf"'
+
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+    p.drawString(100,100, pdf)
+    p.showPage()
+    p.save()
+
+    return buffer.getvalue()
