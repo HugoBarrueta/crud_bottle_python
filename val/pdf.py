f8b0fd9421ddb156ea3b832dcea83bd44f3a660d
Hugo Barrueta <hbarruetaf97@gmail.com>
2019-12-15 13:31:54 -0600

    nuevos cambios

--- val/pdf.py
+++ val/pdf.py
@@ -1,33 +1,7 @@
-from xhtml2pdf import pisa             # import python module
-
-# Define your data
-#sourceHtml = "<html><body><p>To PDF or not to PDF</p></body></html>"
-#outputFilename = "test.pdf"
-
-# Utility function
-def convertHtmlToPdf(sourceHtml, outputFilename):
-    # open output file for writing (truncated binary)
-    resultFile = open(outputFilename, "w+b")
-
-    # convert HTML to PDF
-    pisaStatus = pisa.CreatePDF(
-            sourceHtml,                # the HTML to convert
-            dest=resultFile)           # file handle to recieve result
-
-    # close output file
-    resultFile.close()                 # close output file
-    pisa.startViewer(resultFile)
-    print("pdf Generado")
-    # return True on success and False on errors
-    return pisaStatus.err
-#pisa.showLogging()
-# Main program
-#if __name__ == "__main__":
- #   pisa.showLogging()
-    #convertHtmlToPdf(sourceHtml, outputFilename)
-"""from io import BytesIO
+from io import BytesIO
 from reportlab.pdfgen import canvas
 from bottle import response
+
 def convertHtmlToPdf(pdf):
     response.headers['Content-Type'] = 'application/pdf; charset=UTF-8'
     response.headers['Content-Disposition'] = 'attachment; filename="test.pdf"'
@@ -37,5 +11,6 @@ def convertHtmlToPdf(pdf):
     p.drawString(100,100, pdf)
     p.showPage()
     p.save()
+    buffer.close()
 
-    return buffer.getvalue()"""
+    return buffer.getvalue()
