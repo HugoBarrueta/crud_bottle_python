4c332307ed6348ddfb34127e4453bd97ac2c3e76
Hugo Barrueta <hbarruetaf97@gmail.com>
2019-12-15 02:36:19 -0600

    nuevos requerimientos y code pdf

--- val/pdf.py
+++ val/pdf.py
@@ -1,5 +1,52 @@
-from xhtml2pdf import pisa             # import python module
+import os
+from django.conf import settings
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
+def link_callback(uri, rel):
+    """
+    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+    resources
+    """
+    # use short variable names
+    sUrl = settings.STATIC_URL      # Typically /static/
+    sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
+    mUrl = settings.MEDIA_URL       # Typically /static/media/
+    mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
+
+    # convert URIs to absolute system paths
+    if uri.startswith(mUrl):
+        path = os.path.join(mRoot, uri.replace(mUrl, ""))
+    elif uri.startswith(sUrl):
+        path = os.path.join(sRoot, uri.replace(sUrl, ""))
+    else:
+        return uri  # handle absolute uri (ie: http://some.tld/foo.png)
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+            raise Exception(
+                'media URI must start with %s or %s' % (sUrl, mUrl)
+            )
+    return path
+def render_pdf_view(request):
+    template_path = 'user_printer.html'
+    context = {'myvar': 'this is your template context'}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(Context(context))
+
+    # create a pdf
+    pisaStatus = pisa.CreatePDF(
+       html, dest=response, link_callback=link_callback)
+    # if error then show some funy view
+    if pisaStatus.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
 # Define your data
 #sourceHtml = "<html><body><p>To PDF or not to PDF</p></body></html>"
 #outputFilename = "test.pdf"
@@ -25,17 +72,3 @@ def convertHtmlToPdf(sourceHtml, outputFilename):
 #if __name__ == "__main__":
  #   pisa.showLogging()
     #convertHtmlToPdf(sourceHtml, outputFilename)"""
-from io import BytesIO
-from reportlab.pdfgen import canvas
-from bottle import response
-def convertHtmlToPdf(pdf):
-    response.headers['Content-Type'] = 'application/pdf; charset=UTF-8'
-    response.headers['Content-Disposition'] = 'attachment; filename="test.pdf"'
-
-    buffer = BytesIO()
-    p = canvas.Canvas(buffer)
-    p.drawString(100,100, pdf)
-    p.showPage()
-    p.save()
-
-    return buffer.getvalue()
